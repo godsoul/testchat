ddd81bfba9b084f36d79748477f445b262dcb965
godsoul <godsoul1986@gmail.com>
2014-03-06 20:43:47 +0800

    test

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func ready(w string, sec int64) {
-	time.Sleep(sec * 1e9)
-	fmt.Println(w, "is ready!")
-}
 
 func main() {
-	go ready("Tee", 2)
-	go ready("Coffee", 1)
 	fmt.Println("I'm waiting")
-	time.Sleep(5 * 1e9)
+
 }
